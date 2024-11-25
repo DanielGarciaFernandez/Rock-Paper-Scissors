f6f953e570deb10daaea0fe5f773acad25cf2c12
DanielGraciaFernandez <garferdany@gmail.com>
2024-11-25 19:00:44 +0100

    Variables con los botones añadidas

--- logic.js
+++ logic.js
@@ -1,1 +1,43 @@
-console.log(1);
+function gethumanchoice() {
+
+    let rockButton = document.getElementById("rock");
+    let paperButton = document.getElementById("paper");
+    let scissorsButton = document.getElementById("scissors");
+
+    let mychoice; // Declare mychoice outside of the event handlers
+
+    // Event handlers
+    rockButton.onclick = function() {
+        mychoice = 1; // Choose rock
+        console.log("mychoice:", mychoice); // Log after choice is made
+    };
+
+    paperButton.onclick = function() {
+        mychoice = 2; // Choose paper
+        console.log("mychoice:", mychoice); // Log after choice is made
+    };
+
+    scissorsButton.onclick = function() {
+        mychoice = 3; // Choose scissors
+        console.log("mychoice:", mychoice); // Log after choice is made
+    };
+
+}
+function getcomputerchoice(){
+    let botchoice = Math.floor(Math.random() * 3) + 1;
+    console.log(botchoice);
+    if (botchoice == 1){
+        //mejor usarlo para el humanchoice
+    }
+}
+
+
+
+function playRound(humanchoice, computerchoice) {
+    // your code here!
+  }
+  
+  const humanselection = gethumanchoice();
+  const computerselection = getcomputerchoice();
+  
+  playRound(humanselection, computerselection);
